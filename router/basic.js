e625f2b0d5fa5b0b3a857012d4f5bcdca94a23f0
Harsh <harshvaghasiya21@gmail.com>
2023-08-27 21:16:28 +0530

    updated

--- router/basic.js
+++ router/basic.js
@@ -22,7 +22,7 @@ router.route('/api/nearby').post(controller.nearby);
 router.route('/add_document').get((req,res)=>{
         res.render('add_data');
 })
-router.route('/delete').post(controller.delete);
+router.route('/delete').get(controller.delete);
 router.route('/update').get(controller.renderUpdate).post(controller.update);
 router.route('/login').get(controller.loginpage);
 
